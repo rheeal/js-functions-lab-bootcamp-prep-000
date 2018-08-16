bd28580bce3606f0d1d5890bf76210406b2a6ca8
Allen Rhee <rhee.daehyun@gmail.com>
2018-08-16 21:01:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // write your code below!
-function happyHolidays ()
+function happyHolidays() {
+  return "Happy holidays!";
+};
+ function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+};
+ function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+};
+ function holidayCountdown(holiday, number) {
+  return `It\'s ${number} days until ${holiday}!`;
+};
